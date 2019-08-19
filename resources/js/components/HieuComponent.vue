48aafbd679ff0dd05cd4867331e459e9ab4de2ab
DUONG TRUNG HIEU <hieudt1@bsscommerce.com>
2019-08-19 10:51:48 +0700

    Init Project

---
+++ resources/js/components/HieuComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <span>{{ message }}</span> <br/>
+        <input type="text" v-model="message">
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+      data() {
+          return {
+              message: 'That is hieu',
+              signatures: [],
+              pageCount: 1,
+              endpoint: 'api/signatures?page='
+          };
+        },
+
+        created() {
+        },
+
+        methods: {
+        }
+    }
+</script>
